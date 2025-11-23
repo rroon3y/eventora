9055ec07a83d62d3f8acf913f3144b04de93e851
Rroon3y <rooneyalex2016@gmail.com>
2025-11-23 11:06:21 +0300

    commit

--- src/data/categories.js
+++ src/data/categories.js
@@ -2,22 +2,22 @@ const categories = [
   {
     id: 1,
     name: "Music",
-    icon: "/assets/music.png"
+    icon: "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=100&h=100&fit=crop"
   },
   {
     id: 2,
     name: "Tech",
-    icon: "/assets/tech.png"
+    icon: "https://images.unsplash.com/photo-1518770660439-4636190af475?w=100&h=100&fit=crop"
   },
   {
     id: 3,
     name: "Comedy",
-    icon: "/assets/comedy.png"
+    icon: "https://images.unsplash.com/photo-1541532713592-79a0317b6b77?w=100&h=100&fit=crop"
   },
   {
     id: 4,
     name: "Sports",
-    icon: "/assets/sports.png"
+    icon: "https://images.unsplash.com/photo-1461896836934-ffe607ba8211?w=100&h=100&fit=crop"
   }
 ];
 
