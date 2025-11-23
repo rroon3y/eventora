aa2e1ae0a88a276060b8d7b20b20346d1d78848a
Rroon3y <rooneyalex2016@gmail.com>
2025-11-23 11:06:32 +0300

    commit

--- src/data/events.js
+++ src/data/events.js
@@ -4,21 +4,21 @@ const events = [
     title: "Summer Beats Concert",
     date: "Nov 30, 2025",
     location: "Nairobi, Kenya",
-    image: "/assets/event1.jpg"
+    image: "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?w=400&h=300&fit=crop"
   },
   {
     id: 2,
     title: "Tech Innovators Meetup",
     date: "Dec 5, 2025",
     location: "Mombasa, Kenya",
-    image: "/assets/event2.jpg"
+    image: "https://images.unsplash.com/photo-1511578314322-379afb476865?w=400&h=300&fit=crop"
   },
   {
     id: 3,
     title: "Comedy Night Live",
     date: "Dec 10, 2025",
     location: "Nairobi, Kenya",
-    image: "/assets/event3.jpg"
+    image: "https://images.unsplash.com/photo-1541532713592-79a0317b6b77?w=400&h=300&fit=crop"
   }
 ];
 
