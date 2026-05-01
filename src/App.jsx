7b2a3b41d6801c3b5de25b52c4b9e5465ab00f9f
Rroon3y <rooneyalex2016@gmail.com>
2026-05-01 09:52:50 +0300

    Refactor routing in App component and update CSS imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Events from "./pages/Events";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/events/:id" element={<Events />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
